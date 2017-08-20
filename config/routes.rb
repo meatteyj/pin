b3908408a1a8387d0e2a31deffd0911ce10c700c
James Meattey <meatteyj@wit.edu>
2017-08-20 03:20:07 +0000

    changed routes.rb to include 'users/sign_in'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
   get 'home/contact'
+  get 'users/sign_in'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
